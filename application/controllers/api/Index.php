549d5cca9772463a6df1f81dd9727fbbcdbd5a90
liuran <984653414@qq.com>
2018-09-29 15:53:43 +0800

    1

---
+++ application/controllers/api/Index.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Index extends MY_Controller{
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index(){
+
+    }
+}
