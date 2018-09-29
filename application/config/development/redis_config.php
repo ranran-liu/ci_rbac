fffab6a6c281eb47e3d332dd5fabe31786856535
liuran <984653414@qq.com>
2018-09-29 12:34:25 +0800

    init

---
+++ application/config/development/redis_config.php
@@ -0,0 +1,7 @@
+<?php
+
+$config['redis_host']     = "127.0.0.1";
+$config['redis_port']     = 6379;
+$config['redis_timeout']     = 5;
+$config['redis_auth']     = "123456";
+
---
+++ application/config/production/redis_config.php
@@ -0,0 +1,10 @@
+<?php
+
+$config['redis_host']     = "127.0.0.1";
+$config['redis_port']     = 6379;
+$config['redis_timeout']     = 5;
+$config['redis_auth']     = "liuran@19920311";
+
+//$config['redis_host']     = "redis";
+//$config['redis_port']     = 6379;
+//$config['redis_timeout']     = 5;
