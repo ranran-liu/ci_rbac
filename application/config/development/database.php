801f81adb5ba4019dc0416e433057e879bfac8a7
liuran <984653414@qq.com>
2018-09-29 12:39:49 +0800

    1

--- application/config/development/database.php
+++ application/config/development/database.php
@@ -94,3 +94,25 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+
+$db['admin'] = array(
+    'dsn'	=> '',
+    'hostname' => '127.0.0.1',
+    'username' => 'root',
+    'password' => '123456',
+    'database' => 'admin',
+    'dbdriver' => 'mysqli',
+    'dbprefix' => 'tp_',
+    'pconnect' => FALSE,
+    'db_debug' => (ENVIRONMENT !== 'production'),
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => FALSE,
+    'compress' => FALSE,
+    'stricton' => FALSE,
+    'failover' => array(),
+    'save_queries' => TRUE
+);
