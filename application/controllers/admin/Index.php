bfc0c5d368bfccbe066c122cdf204d8d0e916c0d
liuran <984653414@qq.com>
2018-10-08 16:30:08 +0800

    1

--- application/controllers/admin/Index.php
+++ application/controllers/admin/Index.php
@@ -15,17 +15,8 @@ class Index extends AdminBaseController {
         $this->load->model('Admin/menu_model','menu');
         $list=$this->menu->menu_json();
         $data['SUBMENU_CONFIG'] = $list;
-//        $this->session->set_userdata(array('admin_menu'=>$list));
-        //var_dump($this->session->userdata('userInfo'));
         $this->load->view('admin/boot/index',$data);
     }
-
-    public function index1(){
-        print_r($this->session->userdata());
-    }
-    public function test(){
-        $this->load->model('admin/menu_model','menu');
-        print_r($this->menu->menu_json());
-    }
+    
 
 }
