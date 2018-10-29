fca83d692b90aec144647ea06a8e71e43eaa1fff
liuran <984653414@qq.com>
2018-10-29 18:28:21 +0800

    1

--- application/config/development/mailer.php
+++ application/config/development/mailer.php
@@ -2,8 +2,8 @@
  $config= array (
   'mail_sender' => 'liuran',
   'mail_smtp_server' => 'smtp.163.com',
-  'mail_secure' => 'ssl',
+  'mail_secure' => '',
   'mail_smtp_port' => '25',
   'mail_user' => '15231069699@163.com',
-  'mail_password' => 'liuran19920311',
+  'mail_password' => '12',
 );
--- application/helpers/common_helper.php
+++ application/helpers/common_helper.php
@@ -556,7 +556,7 @@ function sp_send_email($email_user,$subject,$msg,$mail_type='html'){
     $ci->email->message($msg);
     //发送EMAIL. 根据发送结果，成功返回TRUE,失败返回FALSE。
     $res = $ci->email->send();
-    log_message('debug', "回调数据--参数接收====".var_dump($ci->email->print_debugger())."=====".__CLASS__.__METHOD__."-----LINE--".__LINE__."===time:".date("Y-m-d H:i:s",time()));
+    log_message('debug', "回调数据--参数接收====".$ci->email->print_debugger()."=====".__CLASS__.__METHOD__."-----LINE--".__LINE__."===time:".date("Y-m-d H:i:s",time()));
     return $res;
 }
 
